e6869f9414f6d2733ea0e8519031f3ddeff21a25
nhongzhuajainongwonyoung <47553@cru.ac.th>
2022-07-26 14:53:26 +0700

    function

---
+++ work-function.py
@@ -0,0 +1,16 @@
+#คำสั่งหาพื้นที่สี่เหลี่ยม
+def square(width, height):
+    ans = width * height
+    print (ans)
+square(9,18)
+#คำสั่งหาพื้นที่สามเหลี่ยม
+def  triangle(base, height):
+    ans = 1/2 * base * height
+    print (ans)
+triangle(6,14)
+#คำสั่งหาพื้นที่วงกลม
+def circle(radius):
+    ans = 3.14 * (radius * radius)
+    print (ans)
+circle(10)
+#ภรัณยู ผันสันเทียะ 18
